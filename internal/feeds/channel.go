de05b7f998601e97f42468b8e3d363ff77947765
Matteo Crespi <crespi.matteo@icloud.com>
2022-07-01 22:39:14 +0200

    refactor: extract channel into file

---
+++ internal/feeds/channel.go
@@ -0,0 +1,8 @@
+package feeds
+
+import "encoding/xml"
+
+type Channel struct {
+	XMLName xml.Name `xml:"channel"`
+	Items   []Item   `xml:"item"`
+}
--- internal/feeds/rss.go
+++ internal/feeds/rss.go
@@ -6,8 +6,3 @@ type RSS struct {
 	XMLName xml.Name `xml:"rss"`
 	Channel Channel  `xml:"channel"`
 }
-
-type Channel struct {
-	XMLName xml.Name `xml:"channel"`
-	Items   []Item   `xml:"item"`
-}
