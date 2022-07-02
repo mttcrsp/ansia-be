c0f0439bfb3affd1c54baaa10ec3608488317523
Matteo Crespi <crespi.matteo@icloud.com>
2022-07-02 12:22:01 +0200

    chore: mattn/go-sqlite3

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"time"
 
-	"github.com/mttcrsp/ansiabe/internal/articles"
-	"github.com/mttcrsp/ansiabe/internal/core"
-	"github.com/mttcrsp/ansiabe/internal/feeds"
-	"github.com/mttcrsp/ansiabe/internal/rss"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -19,75 +15,82 @@ func main() {
 }
 
 func run() error {
-	newLogger := func(identifier string) log.Logger {
-		logger := log.Logger{}
-		logger.SetOutput(os.Stdout)
-		logger.SetPrefix(fmt.Sprintf("[%s] ", identifier))
-		logger.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-		return logger
+	db, err := sql.Open("sqlite3", "file::memory:?cache=shared")
+	if err != nil {
+		return err
 	}
 
-	fl := feeds.Loader{}
-	rl := rss.Loader{}
-	watcherLogger := newLogger("watcher")
-	watcher := core.NewWatcher(fl, rl)
+	fmt.Println(db)
 
-	extractor := articles.NewExtractor()
-	extractorLogger := newLogger("extractor")
-	queuedExtractor := core.NewQueuedExtractor(*extractor)
+	// newLogger := func(identifier string) log.Logger {
+	// 	logger := log.Logger{}
+	// 	logger.SetOutput(os.Stdout)
+	// 	logger.SetPrefix(fmt.Sprintf("[%s] ", identifier))
+	// 	logger.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	// 	return logger
+	// }
 
-	c := make(chan string)
+	// fl := feeds.Loader{}
+	// rl := rss.Loader{}
+	// watcherLogger := newLogger("watcher")
+	// watcher := core.NewWatcher(fl, rl)
 
-	go func() {
-		watcher.Run(
-			core.WatcherConfig{
-				IterationBackoff: time.Minute,
-			},
-			core.WatcherHandlers{
-				OnInsert: func(wi []core.WatcherItem) {
-					watcherLogger.Println("inserted", len(wi))
+	// extractor := articles.NewExtractor()
+	// extractorLogger := newLogger("extractor")
+	// queuedExtractor := core.NewQueuedExtractor(*extractor)
 
-					var items []rss.Item
-					for _, item := range wi {
-						items = append(items, item.Item)
-					}
-					queuedExtractor.Enqueue(items)
-				},
-				OnDelete: func(wi []core.WatcherItem) {
-					watcherLogger.Println("deleted", len(wi))
-				},
-				OnError: func(err error) {
-					watcherLogger.Println(err)
-				},
-				OnIterationBegin: func() {
-					watcherLogger.Println("iteration did begin")
-				},
-				OnIterationEnd: func() {
-					watcherLogger.Println("iteration did end")
-				},
-			},
-		)
-		c <- "watcher did complete"
-	}()
+	// c := make(chan string)
 
-	go func() {
-		queuedExtractor.Run(
-			core.QueuedExtractorConfig{
-				Backoff: time.Second,
-			},
-			core.QueuedExtractorHandlers{
-				OnItemExtracted: func(qei core.QueuedExtractorItem) {
-					extractorLogger.Printf("extracted item '%s'\n", qei.Item.Link)
-				},
-				OnError: func(err error) {
-					extractorLogger.Println(err)
-				},
-			},
-		)
-		c <- "extractor did complete"
-	}()
+	// go func() {
+	// 	watcher.Run(
+	// 		core.WatcherConfig{
+	// 			IterationBackoff: time.Minute,
+	// 		},
+	// 		core.WatcherHandlers{
+	// 			OnInsert: func(wi []core.WatcherItem) {
+	// 				watcherLogger.Println("inserted", len(wi))
 
-	fmt.Println(<-c)
+	// 				var items []rss.Item
+	// 				for _, item := range wi {
+	// 					items = append(items, item.Item)
+	// 				}
+	// 				queuedExtractor.Enqueue(items)
+	// 			},
+	// 			OnDelete: func(wi []core.WatcherItem) {
+	// 				watcherLogger.Println("deleted", len(wi))
+	// 			},
+	// 			OnError: func(err error) {
+	// 				watcherLogger.Println(err)
+	// 			},
+	// 			OnIterationBegin: func() {
+	// 				watcherLogger.Println("iteration did begin")
+	// 			},
+	// 			OnIterationEnd: func() {
+	// 				watcherLogger.Println("iteration did end")
+	// 			},
+	// 		},
+	// 	)
+	// 	c <- "watcher did complete"
+	// }()
+
+	// go func() {
+	// 	queuedExtractor.Run(
+	// 		core.QueuedExtractorConfig{
+	// 			Backoff: time.Second,
+	// 		},
+	// 		core.QueuedExtractorHandlers{
+	// 			OnItemExtracted: func(qei core.QueuedExtractorItem) {
+	// 				extractorLogger.Printf("extracted item '%s'\n", qei.Item.Link)
+	// 			},
+	// 			OnError: func(err error) {
+	// 				extractorLogger.Println(err)
+	// 			},
+	// 		},
+	// 	)
+	// 	c <- "extractor did complete"
+	// }()
+
+	// fmt.Println(<-c)
 
 	return nil
 }
