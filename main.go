7f931ffbd582b19388a461775caac480e3c38d15
Matteo Crespi <crespi.matteo@icloud.com>
2022-07-02 23:13:59 +0200

    refactor: code hygene

--- main.go
+++ main.go
@@ -22,33 +22,11 @@ func main() {
 }
 
 func run() error {
-	newLogger := func(identifier string) *log.Logger {
-		logger := &log.Logger{}
-		logger.SetOutput(os.Stdout)
-		logger.SetPrefix(fmt.Sprintf("[%s] ", identifier))
-		logger.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-		return logger
-	}
+	feedsLoader := feeds.Loader{}
+	rssLoader := rss.Loader{}
 
-	toItems := func(wis []core.WatcherItem, logger *log.Logger) []core.Item {
-		var cis []core.Item
-		for _, wi := range wis {
-			ci, err := core.NewItem(wi.Item, wi.Feed)
-			if err != nil {
-				logger.Printf("failed to convert item '%s': %s\n", wi.Item.Link, err)
-				continue
-			}
-
-			cis = append(cis, *ci)
-		}
-
-		return cis
-	}
-
-	fl := feeds.Loader{}
-	rl := rss.Loader{}
 	watcherLogger := newLogger("watcher")
-	watcher := core.NewWatcher(fl, rl)
+	watcher := core.NewWatcher(feedsLoader, rssLoader)
 
 	extractor := articles.NewExtractor()
 	extractorLogger := newLogger("extractor")
@@ -57,7 +35,41 @@ func run() error {
 	storeLogger := newLogger("store")
 	store := core.Store{}
 
+	mainFeeds, regionalFeeds, err := feedsLoader.LoadAll()
+	if err != nil {
+		newLogger("feeds").Println("failed to load feeds:", err)
+		return err
+	}
+
+	feedsHandler := server.Feeds(
+		server.FeedsVals{
+			MainFeeds:     mainFeeds,
+			RegionalFeeds: regionalFeeds,
+		},
+	)
+
+	feedBySlugHandler := server.FeedBySlug(
+		server.FeedBySlugVals{
+			MainFeeds:     mainFeeds,
+			RegionalFeeds: regionalFeeds,
+		},
+		server.FeedBySlugDeps{
+			Store: store,
+		},
+	)
+
 	c := make(chan string)
+
+	go func() {
+		gin.SetMode(gin.ReleaseMode)
+
+		r := gin.Default()
+		r.GET("/feeds", feedsHandler)
+		r.GET("/feeds/:feed", feedBySlugHandler)
+		r.Run()
+
+		c <- "server did exit"
+	}()
 
 	go func() {
 		watcher.Run(
@@ -65,24 +77,24 @@ func run() error {
 				IterationBackoff: time.Minute,
 			},
 			core.WatcherHandlers{
-				OnInsert: func(wi []core.WatcherItem) {
-					watcherLogger.Println("inserted", len(wi))
+				OnInsert: func(items []core.WatcherItem) {
+					watcherLogger.Println("inserted", len(items))
 
-					if err := store.InsertItems(toItems(wi, watcherLogger)); err != nil {
+					if err := store.InsertItems(toItems(items, watcherLogger)); err != nil {
 						storeLogger.Println("failed to insert items:", err)
 					}
 					storeLogger.Println("did insert items")
 
 					var rssItems []rss.Item
-					for _, item := range wi {
+					for _, item := range items {
 						rssItems = append(rssItems, item.Item)
 					}
 					queuedExtractor.Enqueue(rssItems)
 				},
-				OnDelete: func(wi []core.WatcherItem) {
-					watcherLogger.Println("deleted", len(wi))
+				OnDelete: func(items []core.WatcherItem) {
+					watcherLogger.Println("deleted", len(items))
 
-					if err := store.DeleteItems(toItems(wi, watcherLogger)); err != nil {
+					if err := store.DeleteItems(toItems(items, watcherLogger)); err != nil {
 						storeLogger.Println("failed to delete items:", err)
 					}
 					storeLogger.Println("did delete items")
@@ -107,14 +119,14 @@ func run() error {
 				Backoff: time.Second / 2,
 			},
 			core.QueuedExtractorHandlers{
-				OnItemExtracted: func(qei core.QueuedExtractorItem) {
-					extractorLogger.Printf("extracted item '%s'\n", qei.Item.Link)
+				OnItemExtracted: func(item core.QueuedExtractorItem) {
+					extractorLogger.Printf("extracted item '%s'\n", item.Item.Link)
 
-					article := core.NewArticle(qei.Article, qei.Item)
+					article := core.NewArticle(item.Article, item.Item)
 					if err := store.InsertArticle(article); err != nil {
 						storeLogger.Printf("failed to insert article '%d': %s\n", article.ItemID, err)
 					}
-					storeLogger.Printf("did insert article '%s'\n", qei.Item.Link)
+					storeLogger.Printf("did insert article '%s'\n", item.Item.Link)
 				},
 				OnError: func(err error) {
 					extractorLogger.Println(err)
@@ -124,37 +136,28 @@ func run() error {
 		c <- "extractor did exit"
 	}()
 
-	mainFeeds, regionalFeeds, err := fl.LoadAll()
-	if err != nil {
-		newLogger("feeds").Println("failed to load feeds:", err)
-		return err
+	return errors.New(<-c)
+}
+
+func newLogger(identifier string) *log.Logger {
+	logger := &log.Logger{}
+	logger.SetOutput(os.Stdout)
+	logger.SetPrefix(fmt.Sprintf("[%s] ", identifier))
+	logger.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	return logger
+}
+
+func toItems(wis []core.WatcherItem, logger *log.Logger) []core.Item {
+	var cis []core.Item
+	for _, wi := range wis {
+		ci, err := core.NewItem(wi.Item, wi.Feed)
+		if err != nil {
+			logger.Printf("failed to convert item '%s': %s\n", wi.Item.Link, err)
+			continue
+		}
+
+		cis = append(cis, *ci)
 	}
 
-	feedHandler := server.FeedBySlug(
-		server.FeedBySlugVals{
-			MainFeeds:     mainFeeds,
-			RegionalFeeds: regionalFeeds,
-		},
-		server.FeedBySlugDeps{
-			Store: store,
-		},
-	)
-
-	feedsHandler := server.Feeds(
-		server.FeedsVals{
-			MainFeeds:     mainFeeds,
-			RegionalFeeds: regionalFeeds,
-		},
-	)
-
-	go func() {
-		gin.SetMode(gin.ReleaseMode)
-		r := gin.Default()
-		r.GET("/feeds", feedsHandler)
-		r.GET("/feeds/:feed", feedHandler)
-		r.Run()
-		c <- "server did exit"
-	}()
-
-	return errors.New(<-c)
+	return cis
 }
